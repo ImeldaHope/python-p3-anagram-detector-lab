9b26588b959c7d7768f3d61a718f64e8502171bd
Imelda Hope <imeldalarusso@gmail.com>
2024-09-01 16:35:37 +0300

    checks for anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    @property
+    def word(self):
+        return self._word 
+
+    @word.setter
+    def word(self, word):
+        self._word = word
+    
+    def match(self, match_list):
+        matched_word = []
+
+        for word in match_list:
+            if sorted(self.word) == sorted(word):
+                matched_word.append(word)          
+                
+        return matched_word
+  
